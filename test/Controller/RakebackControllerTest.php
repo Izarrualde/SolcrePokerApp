1b831661413bae0e7b582be713072be11ac081af
Diego <diego.r.izarrualde@gmail.com>
2019-10-10 15:13:24 -0300

    test(RakebackController, RakebackService): add structure of tests without impementation

---
+++ test/Controller/RakebackControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Solcre\lmsuy\View\JsonView;
+use Solcre\lmsuy\View\View;
+use Test\AppWrapper;
+
+class RakebackControllerTest extends TestCase
+{
+    /*
+    public function createController($view)
+    {
+
+        return $controller;
+    }
+
+    public function testListAll()
+    {
+    	
+    }
+    */
+
+ }
---
+++ test/Service/RakebackServiceTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Solcre\lmsuy\View\JsonView;
+use Solcre\lmsuy\View\View;
+use Test\AppWrapper;
+
+class RakebackServiceTest extends TestCase
+{
+  /*
+	public function testFetchAll()
+	{
+		
+	}
+  */
+
+}
